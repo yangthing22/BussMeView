1bde3e6d43ee4b509ee00fdd353bbd88d1137a4c
yangthing22@gmail.com <yangthing22@gmail.com>
2019-11-11 15:22:29 +0700

    initial commit

---
+++ BussMeView/Commute/CommuteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CommuteViewController.swift
+//  BussMeView
+//
+//  Created by Mulyanti Law on 11/11/19.
+//  Copyright © 2019 Mulyanti Law. All rights reserved.
+//
+
+import UIKit
+
+class CommuteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BussMeView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BussMeView
+//
+//  Created by Mulyanti Law on 11/11/19.
+//  Copyright © 2019 Mulyanti Law. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
