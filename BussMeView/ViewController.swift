38bc1064cbc46f7c73b050e4bc47316767689543
Regnauldy Tanri <regnauldyt@gmail.com>
2019-11-12 14:10:25 +0700

    pop up reminder

--- BussMeView/ViewController.swift
+++ BussMeView/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timerCollectionView.delegate = self
+        timerCollectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 
 }
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 63
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "timerCell", for: indexPath) as! timerCollectionViewCell
+        
+        if indexPath.row < 3 {
+            cell.lblTime.text = ""
+        } else if indexPath.row > 61 {
+            cell.lblTime.text = ""
+        } else {
+            cell.lblTime.text = "\(indexPath.row - 2)"
+        }
+        
+        return cell
+    }
+    
+        
+    
+}
+
 
---
+++ BussMeView/timerCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  timerCollectionViewCell.swift
+//  BussMeView
+//
+//  Created by Regnauldy Tanri on 11/11/19.
+//  Copyright © 2019 Mulyanti Law. All rights reserved.
+//
+
+import UIKit
+
+class timerCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var lblTime: UILabel!
+    
+    
+    
+}
